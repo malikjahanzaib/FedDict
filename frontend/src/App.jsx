bfbba7eb3a96d621ee397226b1e9912db47ec404
Jahanzaib Malik <7zzayb@gmail.com>
2025-02-23 04:30:23 -0500

    Restore toast notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import AdminPage from './components/AdminPage';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import { AuthProvider } from './contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
@@ -30,6 +32,18 @@ function App() {
               </Routes>
             </ErrorBoundary>
           </div>
+          <ToastContainer 
+            position="bottom-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
         </div>
       </Router>
     </AuthProvider>
